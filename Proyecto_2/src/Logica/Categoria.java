b0d59da046f145dcdc5f99e8166011531b7434b0
IamBr4vo <bravo9386@gmail.com>
2023-07-29 18:58:26 -0600

    Se creo la clase categoria con sus atributos

---
+++ Proyecto_2/src/Logica/Categoria.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica;
+
+/**
+ *
+ * @author Bravo
+ */
+public class Categoria {
+    private int id_categoria;
+    private String nombre;
+    private String descripcion;
+
+    public Categoria(int id_categoria, String nombre, String descripcion) {
+        this.id_categoria = id_categoria;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the id_categoria
+     */
+    public int getId_categoria() {
+        return id_categoria;
+    }
+
+    /**
+     * @param id_categoria the id_categoria to set
+     */
+    public void setId_categoria(int id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
