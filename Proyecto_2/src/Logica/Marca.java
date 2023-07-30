e9af13ae555bb5266365b582f7fcc9ecbf1124a8
Johana Solano <solanojohana020@outlook.com>
2023-07-30 17:51:39 -0600

    added the brand class

--- Proyecto_2/src/Logica/Marca.java
+++ Proyecto_2/src/Logica/Marca.java
@@ -19,4 +19,33 @@ public class Marca {
     this.nombre = nombre;
     
     }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
 }
