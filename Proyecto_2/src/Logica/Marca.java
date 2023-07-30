8a764f33e9be5b0cb491b53db0a2c1589ce0fc6b
Johana Solano <solanojohana020@outlook.com>
2023-07-30 17:43:21 -0600

    added the brand class

---
+++ Proyecto_2/src/Logica/Marca.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica;
+
+/**
+ *
+ * @author Johana
+ */
+public class Marca {
+    private int id;
+    private String nombre;
+    
+    
+    
+    public Marca (int id, String nombre){
+    this.id = id;
+    this.nombre = nombre;
+    
+    }
+}
